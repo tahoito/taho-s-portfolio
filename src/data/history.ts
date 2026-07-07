693856d60d1eb9051764ab3321ef07e602cad100
tahoito <taho.ito0304@gmail.com>
2026-07-07 23:59:17 +0800

    hisotyr

---
+++ src/data/history.ts
@@ -0,0 +1,28 @@
+export const histories = [
+  {
+    year: "2008〜",
+    title: "名古屋で生まれる",
+    description: [
+      "小さい頃からパズルや物作りが好き",
+    ],
+    image: "/images/history/2008.jpg",
+  },
+  {
+    year: "2018〜",
+    title: "プログラミングを始める",
+    description: [
+      "小学校では合唱部",
+      "プログラミング教室へ通い始める",
+    ],
+    image: "/images/history/2018.jpg",
+  },
+  {
+    year: "2020〜",
+    title: "KADOKAWA情報工科学院",
+    description: [
+      "将来はプログラマーを目指す",
+      "アプリ開発を本格的に勉強",
+    ],
+    image: "/images/history/2020.jpg",
+  },
+];

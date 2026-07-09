eaaf62863ae97a64c8e9766eab1076fb290e50df
tahoito <taho.ito0304@gmail.com>
2026-07-09 23:07:07 +0800

    英語翻訳

---
+++ src/locales/en.ts
@@ -0,0 +1,26 @@
+export const en = {
+  navbar: {
+    home: "Home",
+    profile: "Profile",
+    portfolio: "Portfolio",
+    works: "Works",
+    skills: "Skills",
+    history: "History",
+    contact: "Contact",
+  },
+
+  about: {
+    welcome: "Welcome to Taho's Portfolio.",
+    explanation1: "Here you'll find the projects I've created and the skills I've developed along the way.",
+    explanation2: "Each project reflects my ideas, growth, and attention to detail. I hope you enjoy exploring my work.",
+  },
+
+  profile: {
+    aboutMe: "I’m an 18-year-old aspiring software engineer from Japan.",
+    workMe: "I’m currently studying Information Technology at APU in Malaysia while developing web applications.",
+    dream1: "My main focus is on Laravel and Java, and I’m passionate about building services that make everyday life more convenient and enjoyable for users. ",
+    dream2: "In my personal projects, I handle everything from planning and UI design to development.",
+    dream3: "My goal is to become a full-stack engineer who creates services used by people around the world.",
+  }
+};
+
